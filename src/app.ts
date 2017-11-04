673387013654849329eae6948ac0e59f121f807c
Emil Ekberg <ekberg.emil@gmail.com>
2017-11-04 12:35:51 +0100

    app.ts should handle startup, extracted server setup to new file

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,25 @@
-import * as express from "express";
-import * as http from "http";
 import * as database from "./database";
-import * as SocketIO from "socket.io";
-import * as path from "path";
-import ConfigInterface from "./interfaces/config";
+import Config from "./interfaces/config";
 import {log, getFileAsJson, getFile} from "./utils";
-import credentialsHandler from "./handlers/credentials-handler";
 import {setPepper} from "./hash-utils";
-const config = getFileAsJson<ConfigInterface>("config.json");
+import startServer from "./server";
+const config = getFileAsJson<Config>("config.json");
 if (!config) {
 	log("config.json does not exist. Create one following the config interface");
 	process.exit(1);
 }
 
-const app = express();
-// TODO: fix this once express is fixed.
-// @ts-ignore
-const server = http.createServer(app);
-const io = SocketIO(server);
-
 if(config.pepper) {
 	setPepper(config.pepper);
 }
 
-database
-	.open(config.mongodb.uri)
-	.then((db) => {
-		try {
-			app.use(express.static(path.resolve(__dirname, config.http.root)));
-			app.get("*", (request, response) => {
-				response.sendFile(path.resolve(__dirname, config.http.root, "index.html"));
-			});
-
-			io.on("connection", (socket) => {
-				log(`socket.io user connected`);
-				credentialsHandler(db, socket);
-			});
-
-			server.listen(config.http.port, () => {
-				log(`http listening on *:${config.http.port}`);
-			});
-		}
-		catch(e) {
-			log(e);
-			database.close();
-		}
-	}).catch((e) => {
-		log(`error! ${e}`);
+(async () => {
+	try {
+		const db = await database.open(config.mongodb.uri);
+		startServer(config, db);
 	}
-);
+	catch(e) {
+		log(`error! ${e}`);
+		database.close();
+	}
+})();
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import * as express from "express";
+import * as http from "http";
+import * as database from "./database";
+import * as SocketIO from "socket.io";
+import * as path from "path";
+import Config from "./interfaces/config";
+import credentialsHandler from "./handlers/credentials-handler";
+import {log} from "./utils";
+import {Db} from "mongodb";
+
+export default function startServer(config: Config, db: Db) {
+	const app = express();
+	// TODO: fix this once express is fixed.
+	// @ts-ignore
+	const server = http.createServer(app);
+	const io = SocketIO(server);
+
+	app.use(express.static(path.resolve(__dirname, config.http.root)));
+	app.get("*", (request, response) => {
+		response.sendFile(path.resolve(__dirname, config.http.root, "index.html"));
+	});
+
+	io.on("connection", (socket) => {
+		log(`socket.io user connected`);
+		credentialsHandler(db, socket);
+	});
+
+	server.listen(config.http.port, () => {
+		log(`http listening on *:${config.http.port}`);
+	});
+}
