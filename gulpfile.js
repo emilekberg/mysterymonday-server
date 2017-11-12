3e63db11db61745bf4a6d420ed6d72bd788e0498
Emil Ekberg <ekberg.emil@gmail.com>
2017-11-12 12:11:02 +0100

    some random edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const compile = require('./gulp/compile');
 
-
 gulp.task('compile', compile);
 gulp.task('watch', ['compile'], function() {
 	gulp.watch('src/**/*.ts', ['compile']);
