ede65f77ac48c2f0e7c50962a034481bca1f784e
Emil Ekberg <ekberg.emil@gmail.com>
2017-11-16 23:28:14 +0100

    cleanup gulp, since it's don't needed for server

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const compile = require('./gulp/compile');
-
-gulp.task('compile', compile);
-gulp.task('watch', ['compile'], function() {
-	gulp.watch('src/**/*.ts', ['compile']);
-});
-gulp.task('default', ['compile']);
